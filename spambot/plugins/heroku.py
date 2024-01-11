858032fbeab99ebe311af0cee40113386df2dde2
TheMafiaBot <98544894+TheMafiaBot@users.noreply.github.com>
2024-01-11 15:31:25 +0530

    Update heroku.py

--- spambot/plugins/heroku.py
+++ spambot/plugins/heroku.py
@@ -17,16 +17,16 @@ from spambot.helpers.neko import paster
 async def restart(e):
     if e.sender_id in MY_USERS:
         await e.delete()
-        if (HEROKU_API_KEY is not None) or (HEROKU_APP_NAME is not None):
+        if (HEROKU_API_KEY is not None) or (HEROKU_APP_ID is not None):
             try:
                 await e.client.send_message(e.chat_id, "`Restarting...`")
                 heroku_conn = heroku3.from_key(HEROKU_API_KEY)
-                app = heroku_conn.app(HEROKU_APP_NAME)
+                app = heroku_conn.app(HEROKU_APP_ID)
                 app.restart()
             except Exception as er:
                 print(er)
         else:
-            await e.client.send_message(e.chat_id, "Please Put `HEROKU_APP_NAME` And `HEROKU_API_KEY` In Heroku Vars!")
+            await e.client.send_message(e.chat_id, "Please Put `HEROKU_APP_ID` And `HEROKU_API_KEY` In Heroku Vars!")
 
 
 @MafiaBot1.on(events.NewMessage(outgoing=True, pattern='/logs'))
@@ -42,10 +42,10 @@ async def restart(e):
 async def restart(e):
     if e.sender_id in MY_USERS:
         await e.delete()
-        if (HEROKU_API_KEY is not None) or (HEROKU_APP_NAME is not None):
+        if (HEROKU_API_KEY is not None) or (HEROKU_APP_ID is not None):
             try:
                 heroku_conn = heroku3.from_key(HEROKU_API_KEY)
-                app = heroku_conn.app(HEROKU_APP_NAME)
+                app = heroku_conn.app(HEROKU_APP_ID)
                 log = app.get_log()
                 logs1 = await paster(log)
                 paste = f"[Click Here To Check Logs]({logs1})"
@@ -53,4 +53,4 @@ async def restart(e):
             except Exception as er:
                 print(er)
         else:
-            await e.client.send_message(e.chat_id, "Please Put `HEROKU_APP_NAME` And `HEROKU_API_KEY` In Heroku Vars!")
+            await e.client.send_message(e.chat_id, "Please Put `HEROKU_APP_ID` And `HEROKU_API_KEY` In Heroku Vars!")
