6b253d9756ad768365103fe83bffc573f9a3eaa9
TheMafiaBot <98544894+TheMafiaBot@users.noreply.github.com>
2024-02-11 02:07:56 +0530

    Add files via upload

--- spambot/config.py
+++ spambot/config.py
@@ -1,8 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config(object):
-    API_ID = os.environ.get("API_ID", None)
-    API_HASH = os.environ.get("API_HASH", None)
+    API_ID = os.environ.get("API_ID", 2184829)
+    API_HASH = os.environ.get("API_HASH", "6930b92388baabff4cb4a1d377085035")
     SESSION_ONE = os.environ.get("SESSION_ONE", None)
     SESSION_TWO = os.environ.get("SESSION_TWO", None)
     SESSION_THREE = os.environ.get("SESSION_THREE", None)
@@ -10,7 +13,7 @@ class Config(object):
     SESSION_FIVE = os.environ.get("SESSION_FIVE", None)
     BOT_TOKEN  = os.environ.get("BOT_TOKEN", None)
     BOT_USERNAME  = os.environ.get("BOT_USERNAME", None)
-    OWNER_ID = os.environ.get("OWNER_ID", None)
+    OWNER_ID = int(os.environ.get("OWNER_ID", None))
     OWNER_NAME = os.environ.get("OWNER_NAME", None)
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
     CO_OWNER_ID = set(int(x) for x in os.environ.get("CO_OWNER_ID", None).split())
@@ -19,3 +22,4 @@ class Config(object):
     HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
     DISPLAY_PIC = os.environ.get("DISPLAY_PIC", None)
     BIO_MSG = os.environ.get("BIO_MSG", None)
+    CMD_HANDLER = os.environ.get("CMD_HANDLER", ".")
---
+++ spambot/exconfig.py
@@ -0,0 +1,23 @@
+import os
+
+class Config(object):
+    API_ID = 2184829
+    API_HASH = "6930b92388baabff4cb4a1d377085035"
+    SESSION_ONE = ""
+    SESSION_TWO = ""
+    SESSION_THREE = ""
+    SESSION_FOUR = ""
+    SESSION_FIVE = ""
+    BOT_TOKEN  = ""
+    BOT_USERNAME  = ""
+    OWNER_ID = ""
+    OWNER_NAME = ""
+    OWNER_USERNAME = ""
+    CO_OWNER_ID = [] 
+    SUDO_USERS = [] 
+    HEROKU_APP_ID = ""
+    HEROKU_API_KEY = ""
+    DISPLAY_PIC = ""
+    BIO_MSG = ""
+    CMD_HANDLER = "."
+
